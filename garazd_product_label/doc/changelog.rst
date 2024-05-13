dc8e45bc546730e7b15d8ce541e2c791dffe2bfd
alihassanmostafa <alihassan.mostafa@gmail.com>
2024-05-13 12:23:51 +0300

    barcode addons list

---
+++ garazd_product_label/doc/changelog.rst
@@ -0,0 +1,63 @@
+.. _changelog:
+
+Changelog
+=========
+
+`15.0.2.6.0`
+------------
+
+- Improve getting of labels to print, add excluding of lines with qty=0.
+
+- Add the "mode" option for technical purposes.
+
+`15.0.2.5.0`
+------------
+
+- Implement sorting of label lines by a product.
+
+- Add the sequence for the label lines.
+
+`15.0.2.4.0`
+------------
+
+- Add the settings section "Product Labels".
+
+- Implement settings to replace the standard Odoo print wizard.
+
+`15.0.2.3.0`
+------------
+
+- Add an ability to choose company for labels in multi-company environment.
+
+`15.0.2.2.0`
+------------
+
+- Rebuild logic to print labels.
+
+`15.0.2.1.0`
+------------
+
+- Add the "company_id" field.
+
+`15.0.2.0.0`
+------------
+
+- Improve the wizard form view.
+
+- Add the "Language" option to translate label fields.
+
+- Add the product label template 50 x 38 m.
+
+`15.0.1.0.1`
+------------
+
+- Add the label option "Border width"
+
+- Add the label option "Human readable barcode"
+
+`15.0.1.0.0`
+------------
+
+- Migration from 14.0
+
+
---
+++ stock_barcodes/models/stock_move_line.py
@@ -0,0 +1,46 @@
+# Copyright 2019 Sergio Teruel <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    barcode_scan_state = fields.Selection(
+        [("pending", "Pending"), ("done", "Done"), ("done_forced", "Done forced")],
+        string="Scan State",
+        default="pending",
+        compute="_compute_barcode_scan_state",
+        readonly=False,
+        store=True,
+    )
+
+    @api.depends("qty_done", "product_uom_qty")
+    def _compute_barcode_scan_state(self):
+        for line in self:
+            if line.qty_done >= line.product_uom_qty:
+                line.barcode_scan_state = "done"
+            else:
+                line.barcode_scan_state = "pending"
+
+    def _barcodes_process_line_to_unlink(self):
+        self.qty_done = 0.0
+
+    def action_barcode_detailed_operation_unlink(self):
+        for sml in self:
+            if sml.product_uom_qty:
+                sml._barcodes_process_line_to_unlink()
+            else:
+                sml.unlink()
+            # HACK: To force refresh wizard values
+            wiz_barcode = self.env["wiz.stock.barcodes.read.picking"].browse(
+                self.env.context.get("wiz_barcode_id", False)
+            )
+            if wiz_barcode.option_group_id.barcode_guided_mode == "guided":
+                wiz_barcode.todo_line_id.line_ids = wiz_barcode.todo_line_id.line_ids
+                if not any(wiz_barcode.todo_line_id.line_ids.mapped("qty_done")):
+                    wiz_barcode.fill_todo_records()
+                    wiz_barcode.determine_todo_action()
+            else:
+                wiz_barcode.fill_todo_records()
+                wiz_barcode.todo_line_id.line_ids = wiz_barcode.todo_line_id.line_ids
