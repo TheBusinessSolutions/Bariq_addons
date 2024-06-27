05b787ca1f9b5086da981056dc113c0d13037b60
alihassanmostafa <alihassan.mostafa@gmail.com>
2024-06-27 16:47:16 +0300

    Update __init__.py

--- stock_line_product_label/__init__.py
+++ stock_line_product_label/__init__.py
@@ -1,9 +1,8 @@
 from odoo import models, fields
 
-
 class StockLinePrintBarcode(models.Model):
     _inherit = ['stock.move', 'stock.move.line']
-    
+
     bales_number = fields.Integer(string="Bales", related='move_id.purchase_line_id.bales_number')
     is_dawar_picking = fields.Boolean(related='picking_id.is_dawar_picking')
 
@@ -12,6 +11,6 @@ class StockLinePrintBarcode(models.Model):
             'product.action_open_label_layout')
         action['context'] = {
             'default_product_ids': [self.product_id.id],
-            'default_custom_quantity': self.quantity_done or 1,
+            'default_custom_quantity': self.bales_number or 1,  # Updated line
         }
         return action
