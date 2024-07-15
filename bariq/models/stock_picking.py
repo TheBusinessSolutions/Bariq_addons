90888063fd8b92598adec616403ca797c6a2c7d1
alihassanmostafa <alihassan.mostafa@gmail.com>
2024-07-15 16:35:08 +0300

    calcluate the absolute weight

--- bariq/models/stock_picking.py
+++ bariq/models/stock_picking.py
@@ -141,7 +141,7 @@ class StockPicking(models.Model):
     @api.onchange('weight_1', 'weight_2')
     def action_calculate_done_qty(self):
         for record in self.move_line_ids_without_package:
-            record.qty_done = self.weight_1 - self.weight_2
+            record.qty_done = abs(self.weight_1 - self.weight_2)
 
 
     @api.onchange('weight_1')
