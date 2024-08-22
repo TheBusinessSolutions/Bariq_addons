6eeac3ac5ad8f2040d8aa742b449bf641dccd91c
alihassanmostafa <alihassan.mostafa@gmail.com>
2024-08-22 14:58:03 +0300

    Update skip_quality_inspection_option.py

--- quality_control_mrp_oca/models/skip_quality_inspection_option.py
+++ quality_control_mrp_oca/models/skip_quality_inspection_option.py
@@ -1,13 +1,5 @@
-from odoo import api, fields, models
-from odoo.exceptions import UserError
+from odoo import fields, models
 
-from odoo.addons.quality_control_oca.models.qc_trigger_line import _filter_trigger_lines
-
-
-
-# Adds a new boolean field to the model, 
-# allowing users to indicate whether quality inspection 
-# should be skipped for a specific bill of materials (BOM).
 class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
