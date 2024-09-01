07183f49c0b74a08a49c96de7b84b215eda9a07a
alihassanmostafa <alihassan.mostafa@gmail.com>
2024-09-01 16:07:30 +0300

    bales with production

---
+++ bariq/models/bales_production.py
@@ -0,0 +1,35 @@
+# Step 1: Add the "received_bales_number" Field to mrp.production
+
+from odoo import models, fields
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    received_bales_number = fields.Integer(string="Received Bales Number")
+
+# Step 3: Pass "received_bales_number" to stock.picking
+from odoo import models, fields, api
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    received_bales_number = fields.Integer(string="Received Bales Number")
+
+    def button_mark_done(self):
+        res = super(MrpProduction, self).button_mark_done()
+        
+        for production in self:
+            if production.received_bales_number:
+                # Find the related stock picking
+                pickings = production.picking_ids.filtered(lambda p: p.state not in ['done', 'cancel'])
+                for picking in pickings:
+                    for move_line in picking.move_line_ids_without_package:
+                        move_line.update({
+                            'received_bales_number': production.received_bales_number,
+                        })
+        return res
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    received_bales_number = fields.Integer(string="Received Bales Number")
