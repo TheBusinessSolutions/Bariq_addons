b21982d13033e157118364731bea5db502587b8c
alihassanmostafa <alihassan.mostafa@gmail.com>
2024-09-03 17:51:05 +0300

    Update skip_quality_inspection_option.py

--- quality_control_mrp_oca/models/skip_quality_inspection_option.py
+++ quality_control_mrp_oca/models/skip_quality_inspection_option.py
@@ -1,6 +1,18 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class MrpBom(models.Model):
     _inherit = "mrp.bom"
 
     skip_quality_inspection = fields.Boolean(string="Skip Quality Inspection")
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    skip_quality_inspection = fields.Boolean(string='Skip Quality Inspection')
+
+    @api.model
+    def create(self, vals):
+        if 'bom_id' in vals:
+            bom = self.env['mrp.bom'].browse(vals['bom_id'])
+            vals['skip_quality_inspection'] = bom.skip_quality_inspection
+        return super(MrpProduction, self).create(vals)
