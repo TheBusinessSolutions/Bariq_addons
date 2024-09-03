f377aacebffab35b885a5ce093dce4cbce551e46
alihassanmostafa <alihassan.mostafa@gmail.com>
2024-09-03 16:37:44 +0300

    upload the original mrp_lot_propagnate

--- bariq/models/bales_production.py
+++ bariq/models/bales_production.py
@@ -5,31 +5,31 @@ from odoo import models, fields
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    received_bales_number = fields.Integer(string="Received Bales Number")
+    production_bales_number = fields.Integer(string="Production Bales Number")
 
-# Step 3: Pass "received_bales_number" to stock.picking
+# Step 3: Pass "production_bales_number" to stock.picking
 
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    received_bales_number = fields.Integer(string="Received Bales Number")
+    production_bales_number = fields.Integer(string="Production Bales Number")
 
     def button_mark_done(self):
         res = super(MrpProduction, self).button_mark_done()
         
         for production in self:
-            if production.received_bales_number:
+            if production.production_bales_number:
                 # Find the related stock picking
                 pickings = production.picking_ids.filtered(lambda p: p.state not in ['done', 'cancel'])
                 for picking in pickings:
                     for move_line in picking.move_line_ids_without_package:
                         move_line.update({
-                            'received_bales_number': production.received_bales_number,
+                            'production_bales_number': production.production_bales_number,
                         })
         return res
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    received_bales_number = fields.Integer(string="Received Bales Number")
+    production_bales_number = fields.Integer(string="Production Bales Number")
