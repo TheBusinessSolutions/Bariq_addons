7dc804e7677bd26321c1967281aef8bdb1014517
alihassanmostafa <alihassan.mostafa@gmail.com>
2024-09-08 14:37:05 +0300

    Update skip_quality_inspection_option.py

--- quality_control_mrp_oca/models/skip_quality_inspection_option.py
+++ quality_control_mrp_oca/models/skip_quality_inspection_option.py
@@ -5,6 +5,7 @@ class MrpBom(models.Model):
 
     skip_quality_inspection = fields.Boolean(string="Skip Quality Inspection")
 
+
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
@@ -12,7 +13,28 @@ class MrpProduction(models.Model):
 
     @api.model
     def create(self, vals):
-        if 'bom_id' in vals:
+        if vals.get('bom_id'):
+            # Ensure that the BOM exists and is a valid record
             bom = self.env['mrp.bom'].browse(vals['bom_id'])
-            vals['skip_quality_inspection'] = bom.skip_quality_inspection
+            if bom.exists():
+                vals['skip_quality_inspection'] = bom.skip_quality_inspection
         return super(MrpProduction, self).create(vals)
+
+# from odoo import models, fields, api
+
+# class MrpBom(models.Model):
+#     _inherit = "mrp.bom"
+
+#     skip_quality_inspection = fields.Boolean(string="Skip Quality Inspection")
+
+# class MrpProduction(models.Model):
+#     _inherit = 'mrp.production'
+
+#     skip_quality_inspection = fields.Boolean(string='Skip Quality Inspection')
+
+#     @api.model
+#     def create(self, vals):
+#         if 'bom_id' in vals:
+#             bom = self.env['mrp.bom'].browse(vals['bom_id'])
+#             vals['skip_quality_inspection'] = bom.skip_quality_inspection
+#         return super(MrpProduction, self).create(vals)
