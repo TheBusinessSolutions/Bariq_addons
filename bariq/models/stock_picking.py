911542d1e07fd503d2016737c5fdec6cd306d05a
alihassanmostafa <alihassan.mostafa@gmail.com>
2024-09-16 15:06:44 +0300

    adding the generate bales list to Picking

--- bariq/models/stock_picking.py
+++ bariq/models/stock_picking.py
@@ -18,6 +18,30 @@ class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
 
     bales_number = fields.Integer(string='Bales Number')
+    # One2many relation to store bales
+    #this is the bales list which will be generated in the picking
+    bales_ids = fields.One2many('stock.picking.bale', 'picking_id', string="Bales Ref")
+
+
+    # Button to generate the bales based on the bales_number field
+def action_generate_bales(self):
+    self.ensure_one()
+    if not self.bales_ids:  # Ensure we don't duplicate the bales
+        bales_list = []
+        for i in range(1, self.bales_number + 1):
+            bales_list.append((0, 0, {
+                'name': 'BL' + str(i),
+            }))
+        self.bales_ids = bales_list
+    return True
+
+class Bale(models.Model):
+    _name = 'stock.picking.bale'
+    _description = 'Bale'
+
+    name = fields.Char(string="Bale Number", required=True)
+    picking_id = fields.Many2one('stock.picking', string="Picking")
+
 
 
 class StockPicking(models.Model):
--- bariq/models/stock_picking_generate_bales.py
+++ bariq/models/stock_picking_generate_bales.py
@@ -1,26 +1,26 @@
 from odoo import models, fields, api
 
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+# class StockPicking(models.Model):
+#     _inherit = 'stock.picking'
     
-    # One2many relation to store bales
-    bales_ids = fields.One2many('stock.picking.bale', 'picking_id', string="Bales Ref")
+#     # One2many relation to store bales
+#     bales_ids = fields.One2many('stock.picking.bale', 'picking_id', string="Bales Ref")
     
-    # Button to generate the bales based on the bales_number field
-    def action_generate_bales(self):
-        self.ensure_one()
-        if not self.bales_ids:  # Ensure we don't duplicate the bales
-            bales_list = []
-            for i in range(1, self.bales_number + 1):
-                bales_list.append((0, 0, {
-                    'name': 'BL' + str(i),
-                }))
-            self.bales_ids = bales_list
-        return True
+#     # Button to generate the bales based on the bales_number field
+#     def action_generate_bales(self):
+#         self.ensure_one()
+#         if not self.bales_ids:  # Ensure we don't duplicate the bales
+#             bales_list = []
+#             for i in range(1, self.bales_number + 1):
+#                 bales_list.append((0, 0, {
+#                     'name': 'BL' + str(i),
+#                 }))
+#             self.bales_ids = bales_list
+#         return True
 
-class Bale(models.Model):
-    _name = 'stock.picking.bale'
-    _description = 'Bale'
+# class Bale(models.Model):
+#     _name = 'stock.picking.bale'
+#     _description = 'Bale'
 
-    name = fields.Char(string="Bale Number", required=True)
-    picking_id = fields.Many2one('stock.picking', string="Picking")
+#     name = fields.Char(string="Bale Number", required=True)
+#     picking_id = fields.Many2one('stock.picking', string="Picking")
