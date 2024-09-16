72f5a5d31aecaac33cee6519af5a439a0f05893e
alihassanmostafa <alihassan.mostafa@gmail.com>
2024-09-16 15:13:54 +0300

    Update stock_picking.py

--- bariq/models/stock_picking.py
+++ bariq/models/stock_picking.py
@@ -24,16 +24,16 @@ class StockProductionLot(models.Model):
 
 
     # Button to generate the bales based on the bales_number field
-def action_generate_bales(self):
-    self.ensure_one()
-    if not self.bales_ids:  # Ensure we don't duplicate the bales
-        bales_list = []
-        for i in range(1, self.bales_number + 1):
-            bales_list.append((0, 0, {
-                'name': 'BL' + str(i),
-            }))
-        self.bales_ids = bales_list
-    return True
+    def action_generate_bales(self):
+        self.ensure_one()
+        if not self.bales_ids:  # Ensure we don't duplicate the bales
+            bales_list = []
+            for i in range(1, self.bales_number + 1):
+                bales_list.append((0, 0, {
+                    'name': 'BL' + str(i),
+                }))
+            self.bales_ids = bales_list
+        return True
 
 class Bale(models.Model):
     _name = 'stock.picking.bale'
